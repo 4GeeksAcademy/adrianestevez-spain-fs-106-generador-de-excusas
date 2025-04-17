616c3c00260f8c1afb2e774555d96665babd0226
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-17 15:31:35 +0200

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
+let excuseBox = document.querySelector("#excusa");
+let reloadBtn = document.querySelector("#reload-button");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["Un plátano gigante", "Una abuela ninja", "El repartidor de pizza", "El vecino"];
+let action = ["hipnotizó", "secuestró", "se sentó encima de", "se comió"];
+let what = ["mi planta carnívora", "mi perro", "mis deberes", "mi colección de calcetines"];
+let when = ["mientras cantaba en la ducha.", "durante una batalla de breakdance.", "durante el desfile de patos.", "justo antes de salir de casa."];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const randomArrElement = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+window.onload = () => {
+  excuseBox.innerHTML = `${randomArrElement(who)} ${randomArrElement(action)} ${randomArrElement(what)} ${randomArrElement(when)}`;
+}
+
+reloadBtn.addEventListener("click", () => {
+  window.location.reload();
+});
